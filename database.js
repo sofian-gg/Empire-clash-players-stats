65fecb291905e7c45d425f704d1743716829b069
sofian-gg <qwertyuiopsdfghjklazxcv@gmail.com>
2026-03-16 04:53:57 +0200

    Create database.js with soldier statistics
    Added a database of verified soldiers with their stats.

---
+++ database.js
@@ -0,0 +1,29 @@
+// أرشيف الإمبراطورية - 10 سجلات جاهزة للتعديل
+// لاستبدال لاعب: غير الاسم بين القوسين "" وغير الأرقام ببياناته الحقيقية
+const verifiedSoldiers = {
+    "sofian_gg": { 
+        kills: "15,420", deaths: "2,100", gold: "1.2M", crowns: "500", 
+        wins: "300", losses: "50", time: "420h", buried: "1,500" 
+    },
+    
+    "player_test_1": { kills: "100", deaths: "50", gold: "5K", crowns: "1", wins: "3", losses: "1", time: "2h", buried: "5" },
+    
+    "player_test_2": { kills: "250", deaths: "80", gold: "15K", crowns: "4", wins: "10", losses: "3", time: "8h", buried: "20" },
+    
+    "player_test_3": { kills: "500", deaths: "120", gold: "30K", crowns: "10", wins: "20", losses: "5", time: "15h", buried: "45" },
+    
+    "player_test_4": { kills: "1,000", deaths: "300", gold: "80K", crowns: "15", wins: "35", losses: "10", time: "40h", buried: "100" },
+    
+    "player_test_5": { kills: "2,000", deaths: "450", gold: "150K", crowns: "30", wins: "60", losses: "15", time: "80h", buried: "250" },
+    
+    "player_test_6": { kills: "4,500", deaths: "900", gold: "500K", crowns: "100", wins: "200", losses: "40", time: "200h", buried: "800" },
+    
+    "player_test_7": { kills: "Unknown", deaths: "Unknown", gold: "Unknown", crowns: "Unknown", wins: "Unknown", losses: "Unknown", time: "Unknown", buried: "Unknown" },
+    
+    "player_test_8": { kills: "Unknown", deaths: "Unknown", gold: "Unknown", crowns: "Unknown", wins: "Unknown", losses: "Unknown", time: "Unknown", buried: "Unknown" },
+    
+    "player_test_9": { kills: "Unknown", deaths: "Unknown", gold: "Unknown", crowns: "Unknown", wins: "Unknown", losses: "Unknown", time: "Unknown", buried: "Unknown" },
+    
+    "player_test_10": { kills: "Unknown", deaths: "Unknown", gold: "Unknown", crowns: "Unknown", wins: "Unknown", losses: "Unknown", time: "Unknown", buried: "Unknown" }
+};
+
